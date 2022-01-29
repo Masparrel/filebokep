22f27c85cdf39eda6c56de1e4433ef65619c5831
Masparrel <98620749+Masparrel@users.noreply.github.com>
2022-01-30 05:17:46 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -108,7 +108,7 @@ class Bot(Client):
         if FORCE_SUB_GROUP:
             try:
                 link = await self.export_chat_invite_link(FORCE_SUB_GROUP)
-                self.invitelink = link
+                self.invitelink4 = link
             except Exception as a:
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning(
