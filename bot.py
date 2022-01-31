6300c8b5caec21f642878875633a4317cc77cf43
Masparrel <98620749+Masparrel@users.noreply.github.com>
2022-02-01 04:42:52 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -60,7 +60,7 @@ class Bot(Client):
         if FORCE_SUB_CHANNEL1:
             try:
                 link = await self.export_chat_invite_link(FORCE_SUB_CHANNEL1)
-                self.invitelink5 = link
+                self.invitelink1 = link
             except Exception as a:
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning(
